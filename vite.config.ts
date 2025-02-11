5fb6340325c912fe556533f6450d778280d9513f
Bohdan Ohorodnii <limposfeed@gmail.com>
2025-02-11 17:24:59 +0200

    fix: build errors 2.1

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,14 @@ export default defineConfig(({ mode }) => ({
   } : {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
   resolve: {
     alias: {
